a37fcd5022b2af480b055c7af801bed84e782d44
sourabh <raisourabh85@gmail.com>
2023-11-06 11:45:13 +0530

    userModule completed

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { activateUser,  getUserInfo,  loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken } from "../controllers/user.controller";
+import { activateUser,  getUserInfo,  loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateProfilePicture, updateUserInfo } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
@@ -10,5 +10,8 @@ userRouter.get('/logout',isAuthenticated,logoutUser)
 userRouter.get('/refresh',updateAccessToken)
 userRouter.get('/me',isAuthenticated,getUserInfo)
 userRouter.post('/social-auth',socialAuth)
+userRouter.put('/update-user-info',isAuthenticated,updateUserInfo)
+userRouter.put('/update-user-password',isAuthenticated,updatePassword)
+userRouter.put('/update-user-avatar',isAuthenticated,updateProfilePicture)
 
 export default userRouter;
